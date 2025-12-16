147962b6e6730b9803f5d2a1413929c156d1cb37
Toomas Tiganik <135324563+toomastiganik90@users.noreply.github.com>
2025-12-16 20:30:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const ingredientRouter = require(`./routes/ingredients.routes`);
 const fullRecipesRouter = require(`./routes/fullRecipes.routes`);
 const randomRouter = require(`./routes/randomRecipe.routes`);
 
-app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
+app.use(express.json());
 app.use(`/recipes`, recipeRouter);
 app.use(`/ingredients`, ingredientRouter);
 app.use(`/fullrecipes`, fullRecipesRouter);
